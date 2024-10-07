f7053e4dc1ed35852819c447129144ff10a07b13
vihanvasusen <165189659+vihanvasusen@users.noreply.github.com>
2024-10-07 18:43:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Initialize carousel and set interval for automatic sliding
+var heroSlider = document.getElementById('heroSlider');
+var carousel = new bootstrap.Carousel(heroSlider, {
+    interval: 3000, // 3000ms = 3 seconds
+    ride: 'carousel'
+});
+
+
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
